3f88adbac540b67a0c0e044f280b01e8e77f010f
adebo i <ireality@adebos-MacBook-Pro.local>
2019-03-26 22:56:49 -0400

    Initial commit

---
+++ src/main/java/com/xife/wdce/WdceApplication.java
@@ -0,0 +1,13 @@
+package com.xife.wdce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdceApplication.class, args);
+    }
+
+}
